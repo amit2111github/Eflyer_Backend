792b1610f8dc5545f8cef2c7737ae0a126176a75
Amit Kumar <amitkumarmandal2111@gmail.com>
2021-11-19 21:40:34 +0530

    Email Sender

--- controllers/auth.js
+++ controllers/auth.js
@@ -5,20 +5,43 @@ const { body, validationResult } = require('express-validator');
 const jwt = require('jsonwebtoken');
 const expressJwt = require('express-jwt');
 const { secret } = require('../config/vars');
+const nodemailer = require('nodemailer');
+//
 
-exports.signOut = (req, res, next) => {
+https: exports.signOut = (req, res, next) => {
 	res.clearCookie('token');
 	return res.status(200).json({ msg: 'User sign out successfully' });
 };
 
+const sendMail = async (name, email) => {
+	try {
+		let transporter = nodemailer.createTransport({
+			service: 'gmail',
+			auth: {
+				user: 'amit.dev.nit@gmail.com',
+				pass: 'AmitMandal@2111',
+			},
+		});
+		let info = await transporter.sendMail({
+			from: '"Eflyer 👻" <amit.dev.nit@gmail.com>',
+			to: email,
+			subject: 'Hello ✔',
+			html: `<p>Hello</p> <h2>${name}</h2> </br> <p>Welcome to the Eflyer Hope you will enjoy in shopping.</p></br><img src = "res.cloudinary.com/dnd5dhyzv/image/upload/v1636368072/Logo/logo_wmff3n.png">`,
+		});
+		const response = await transporter.sendMail(info);
+	} catch (err) {
+		console.log(err);
+	}
+};
 exports.signUp = (req, res, next) => {
 	const errors = validationResult(req);
 	if (!errors.isEmpty()) {
 		return res.status(400).json({ errors: errors.array() });
 	}
 	const user = new User(req.body);
-	user.save((err, data) => {
+	user.save(async (err, data) => {
 		if (err) return res.status(400).json({ error: 'NOT able to save user in DB' });
+		await sendMail(user.name, user.email);
 		return res.json({
 			id: user._id,
 			name: user.name,
--- routes/auth.js
+++ routes/auth.js
@@ -1,34 +1,30 @@
-var express = require("express");
-const { body, validationResult } = require("express-validator");
+var express = require('express');
+const { body, validationResult } = require('express-validator');
 var router = express.Router();
-const User = require("../models/user.js");
-const { signOut, signUp, signIn, isSignedIn } = require("../controllers/auth");
+const User = require('../models/user.js');
+const { signOut, signUp, signIn, isSignedIn } = require('../controllers/auth');
 const user = require('./users');
 /* GET home page. */
 
-router.get("/signout", signOut);
+router.get('/signout', signOut);
 
 router.post(
-  "/signup",
-  [
-    body("name")
-      .isLength({ min: 3 })
-      .withMessage("Name should be at least 3 chars long"),
-    body("email").isEmail().withMessage("Email is required"),
-    body("password")
-      .isLength({ min: 6 })
-      .withMessage("Password should be at least 6 chars long"),
-  ],
-  signUp
+	'/signup',
+	[
+		body('name').isLength({ min: 3 }).withMessage('Name should be at least 3 chars long'),
+		body('email').isEmail().withMessage('Email is required'),
+		body('password').isLength({ min: 6 }).withMessage('Password should be at least 6 chars long'),
+	],
+	signUp
 );
 
 router.post(
-  "/signin",
-  [
-    body("email").isEmail().withMessage("Email is required"),
-    body("password").isLength({ min: 1 }).withMessage("Password is required"),
-  ],
-  signIn
+	'/signin',
+	[
+		body('email').isEmail().withMessage('Email is required'),
+		body('password').isLength({ min: 1 }).withMessage('Password is required'),
+	],
+	signIn
 );
 
 // router.get("/testroute", isSignedIn, (req, res, next) => {
